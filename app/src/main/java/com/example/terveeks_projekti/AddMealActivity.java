57ce2a3793e97d085407f8d19a6945f9d3c74063
davf591 <davidkullonen@gmail.com>
2021-04-28 11:51:00 +0300

    muutin layout:tia, ja lisäsin yhden lauseen koodia.

--- app/src/main/java/com/example/terveeks_projekti/AddMealActivity.java
+++ app/src/main/java/com/example/terveeks_projekti/AddMealActivity.java
@@ -3,6 +3,7 @@ package com.example.terveeks_projekti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddMealActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class AddMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
     }
+
+
+    //nappula?
+    public void plus(View view){
+
+    }
+
+
+
+
 }
