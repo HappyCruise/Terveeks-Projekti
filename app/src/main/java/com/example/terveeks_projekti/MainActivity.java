232282fdc3f36e6773dc7e18c33af8badebca86e
Leevi Laukkanen <leevi.laukkanen@metropolia.fi>
2021-04-30 13:36:58 +0300

    Added all the code in AddExerciseActivity.java and activity_view_exercise.xml

--- app/src/main/java/com/example/terveeks_projekti/MainActivity.java
+++ app/src/main/java/com/example/terveeks_projekti/MainActivity.java
@@ -3,9 +3,21 @@ package com.example.terveeks_projekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateCaloriesShown();
     }
 
     /**Called when the user clicks the Add User button*/
@@ -31,5 +44,47 @@ public class MainActivity extends AppCompatActivity {
     public void addExercise(View view){
         Intent intent = new Intent(this, AddExerciseActivity.class);
         startActivity(intent);
+    }
+
+    /**Called when the user clicks the Show Exercises button */
+    public void showExercises(View view){
+        Intent intent = new Intent(this, ShowExercisesActivity.class);
+        startActivity(intent);
+    }
+
+    public int getCaloriesToday(){
+        int calorieCount =0;
+
+        //Get the data and map it.
+        SharedPreferences prefGet = getSharedPreferences("getExercises", Activity.MODE_PRIVATE);
+        Map<String, ?> keys = prefGet.getAll();
+
+
+        //Get current date
+        String currDate = new SimpleDateFormat("dd / MM / yyyy", Locale.getDefault()).format(new Date());
+
+        //Go through the mapped data
+        for(Map.Entry<String,?> entry: keys.entrySet()){
+            //Split the key with "-"
+            String strKey = String.valueOf(entry.getKey());
+            String[] arrOfStrKey = strKey.split("-",2);
+
+            //Split the value with "-"
+            String strValue = String.valueOf(entry.getValue());
+            String[] arrOfStrValue = strValue.split("-",2);
+
+            //If key Starts with "calories" add its value to the current calorie count
+            if(arrOfStrKey[0].equals("CALORIES")){
+                if(arrOfStrValue[0].equals(currDate)){
+                    calorieCount += Integer.parseInt(arrOfStrValue[1]);
+                }
+            }
+        }
+        return calorieCount;
+    }
+
+    public void updateCaloriesShown(){
+        TextView tvCalories = findViewById(R.id.tvBurnedCaloriesToday);
+        tvCalories.setText("Tänään poltetut kalorit: "+ getCaloriesToday());
     }
 }
---
+++ app/src/main/java/com/example/terveeks_projekti/ShowExercisesActivity.java
@@ -0,0 +1,66 @@
+package com.example.terveeks_projekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+public class ShowExercisesActivity extends AppCompatActivity {
+
+    ArrayList<String> exercises = new ArrayList<String>();
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_exercises);
+
+        //ListView initialization
+        lv = findViewById(R.id.lvExercises);
+
+        getValues();
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,
+                exercises
+        );
+
+        lv.setAdapter(arrayAdapter);
+
+
+
+    }
+
+    private void getValues(){
+        //Get the data and map it
+        SharedPreferences prefGet = getSharedPreferences("getExercises", Activity.MODE_PRIVATE);
+        Map<String,?> keys = prefGet.getAll();
+
+        //Get current date
+        String currDate = new SimpleDateFormat("dd / MM / yyyy", Locale.getDefault()).format(new Date());
+
+        //Go through the mapped data
+        for(Map.Entry<String,?> entry: keys.entrySet()){
+            //Split the key from "-"
+            String strKey = String.valueOf(entry.getKey());
+            String[] arrOfKey = strKey.split("-",2);
+
+            //If the key does NOT start with "CALORIES" add it to the display list <exercises>.
+            if(!arrOfKey[0].equals("CALORIES")){
+                String currString = entry.getValue().toString();
+                exercises.add(currString);
+            }
+        }
+    }
+}
